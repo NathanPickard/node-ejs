11dd34d1f77440858a4a00931af77fef853b0aa3
Nathan Pickard <nathan.pickard87@gmail.com>
2017-02-03 15:44:44 -0800

    Adding EJS templates

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var expressLayouts = require('express-ejs-layouts');
 var app = express();
 var port = 8080;
-var router = require('./app/routes');
 
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
+var router = require('./app/routes');
 app.use('/', router);
+
 
 app.use(express.static(__dirname + '/public'));
 
